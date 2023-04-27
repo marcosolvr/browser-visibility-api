d1389c889b476029e8a88b62f594aaf1212fde16
Marcos Abreu <marcos.abreu.oliveira@gmail.com>
2023-04-27 16:58:40 -0300

    start repository

---
+++ src/visibility-api.ts
@@ -0,0 +1,20 @@
+export function trackUserVisibility(): void {
+  localStorage.getItem('lastWindowVisibleTimestamp') ? handleVisibilityChange() : null
+
+  if (document.addEventListener) document.addEventListener('visibilitychange', handleVisibilityChange)
+}
+
+function handleVisibilityChange(): void {
+  if (document.visibilityState === 'hidden') {
+    localStorage.setItem('lastWindowVisibleTimestamp', String(Date.now()))
+  }
+
+  if (document.visibilityState === 'visible') {
+    const lastWindowVisibleTimestamp: number = new Date(Number(localStorage.getItem('lastWindowVisibleTimestamp'))).getTime()
+    const fiftyHoursAfterTimestamp: number = lastWindowVisibleTimestamp + (2 * 60 * 1000)
+
+    if (Date.now() >= fiftyHoursAfterTimestamp) {
+      alert('você passou muito tempo inativo!')
+    }
+  }
+}
