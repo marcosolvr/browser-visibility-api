5b8e888468a10c708f064451a8682610a3e4ee13
Marcos Abreu <marcos.abreu.oliveira@gmail.com>
2023-05-30 08:53:39 -0300

    refact: run prettier

--- src/main.ts
+++ src/main.ts
@@ -17,9 +17,9 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
+    <a href="/another-page" class="read-the-docs">
+      Link to another page
+    </a>
   </div>
 `
 
--- src/visibility-api.ts
+++ src/visibility-api.ts
@@ -1,20 +1,22 @@
 export function trackUserVisibility(): void {
-  localStorage.getItem('lastWindowVisibleTimestamp') ? handleVisibilityChange() : null
-
-  if (document.addEventListener) document.addEventListener('visibilitychange', handleVisibilityChange)
+  if (document.addEventListener)
+    document.addEventListener("visibilitychange", handleVisibilityChange);
 }
 
 function handleVisibilityChange(): void {
-  if (document.visibilityState === 'hidden') {
-    localStorage.setItem('lastWindowVisibleTimestamp', String(Date.now()))
+  if (document.visibilityState === "hidden") {
+    localStorage.setItem("lastWindowVisibleTimestamp", String(Date.now()));
   }
 
-  if (document.visibilityState === 'visible') {
-    const lastWindowVisibleTimestamp: number = new Date(Number(localStorage.getItem('lastWindowVisibleTimestamp'))).getTime()
-    const fiftyHoursAfterTimestamp: number = lastWindowVisibleTimestamp + (2 * 60 * 1000)
+  if (document.visibilityState === "visible") {
+    const lastWindowVisibleTimestamp: number = new Date(
+      Number(localStorage.getItem("lastWindowVisibleTimestamp"))
+    ).getTime();
+    const fiftyHoursAfterTimestamp: number =
+      lastWindowVisibleTimestamp + 2 * 60 * 1000;
 
     if (Date.now() >= fiftyHoursAfterTimestamp) {
-      alert('você passou muito tempo inativo!')
+      alert("você passou muito tempo inativo!");
     }
   }
 }
